d4af874b8dc0f3a7f9c9ab49f5e986a291d628bf
Gyps0phila <chenjie.on1@qq.com>
2016-01-25 10:59:04 +0800

    Initial commit

---
+++ src/main/java/com/example/gypsophila/dailytest/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.gypsophila.dailytest;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.gypsophila.scrollview.ScrollViewAty;
+import com.example.gypsophila.viewflipper.ViewFlipperAty;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private Button fragment,viewPager,btn_viewFlipper,btn_scrollView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragment = (Button) findViewById(R.id.fragment);
+        viewPager = (Button) findViewById(R.id.viewPager);
+        btn_viewFlipper = (Button) findViewById(R.id.btn_viewFlipper);
+        btn_scrollView = (Button) findViewById(R.id.btn_scrollView);
+        viewPager.setOnClickListener(this);
+        btn_viewFlipper.setOnClickListener(this);
+        btn_scrollView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fragment: {
+                break;
+            }
+            case R.id.viewPager: {
+                Intent intent = new Intent(this, ViewPagerAty.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.btn_viewFlipper: {
+                Intent intent = new Intent(this, ViewFlipperAty.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.btn_scrollView: {
+                Intent intent = new Intent(this, ScrollViewAty.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
+    }
+}
---
+++ src/main/java/com/example/gypsophila/scrollview/ScrollViewAty.java
@@ -0,0 +1,21 @@
+package com.example.gypsophila.scrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.gypsophila.dailytest.R;
+
+/**
+ * Created by Gypsophila on 2016/1/24.
+ */
+public class ScrollViewAty extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scrollview);
+
+
+
+    }
+}
