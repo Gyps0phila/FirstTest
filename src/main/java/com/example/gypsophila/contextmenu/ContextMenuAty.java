f71fd0efcb9d179557266fc969638c580f7c5edf
Gyps0phila <chenjie.on1@qq.com>
2016-02-03 14:51:08 +0800

    optionmenu contextmenu submenu

---
+++ src/main/java/com/example/gypsophila/contextmenu/ContextMenuAty.java
@@ -0,0 +1,117 @@
+package com.example.gypsophila.contextmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.gypsophila.dailytest.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Gypsophila on 2016/2/3.
+ */
+public class ContextMenuAty extends Activity {
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contextmenu);
+        showListView();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        /**
+         * 代码动态加载方式
+         */
+        //设置menu显示内容
+        menu.setHeaderIcon(R.mipmap.item4);
+        menu.setHeaderTitle("context operation");
+//        menu.add(1, 1, 1, "复制");
+//        menu.add(1, 1, 1, "粘贴");
+//        menu.add(1, 1, 1, "剪切");
+//        menu.add(1, 1, 1, "重命名");
+
+        /**
+         * 加载xml
+         */
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_context, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        /**
+         * 代码动态加载
+         */
+//        switch (item.getItemId()) {
+//            case 1: {
+//                Toast.makeText(this, "点击了复制", Toast.LENGTH_SHORT).show();
+//                break;
+//            }
+//            case 2: {
+//                Toast.makeText(this, "点击了粘贴", Toast.LENGTH_SHORT).show();
+//                break;
+//            }
+//            case 3: {
+//                Toast.makeText(this, "点击了剪切", Toast.LENGTH_SHORT).show();
+//                break;
+//            }
+//            case 4: {
+//                Toast.makeText(this, "点击了重命名", Toast.LENGTH_SHORT).show();
+//                break;
+//            }
+//        }
+
+        switch (item.getItemId()) {
+            case R.id.context_menu_item1: {
+                Toast.makeText(this, "点击了复制", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.context_menu_item2: {
+                Toast.makeText(this, "点击了粘贴", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.context_menu_item3: {
+                Toast.makeText(this, "点击了剪切", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.context_menu_item4: {
+                Toast.makeText(this, "点击了重命名", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+
+
+
+        return super.onContextItemSelected(item);
+    }
+
+    private void showListView() {
+
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getData());
+        listView.setAdapter(arrayAdapter);
+        this.registerForContextMenu(listView);
+    }
+
+    private List<String> getData() {
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < 5; i++) {
+            list.add("file" + (i + 1));
+        }
+        return list;
+    }
+}
--- src/main/java/com/example/gypsophila/dailytest/MainActivity.java
+++ src/main/java/com/example/gypsophila/dailytest/MainActivity.java
@@ -4,22 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.gypsophila.contextmenu.ContextMenuAty;
 import com.example.gypsophila.notification.NotificationAty;
 import com.example.gypsophila.dialog.DialogAty;
 import com.example.gypsophila.gallery.GalleryAty;
 import com.example.gypsophila.scrollview.ScrollViewAty;
 import com.example.gypsophila.seekbar.SeekBarAty;
+import com.example.gypsophila.sharedpreferences.SharedPreferencesAty;
 import com.example.gypsophila.toast.ToastAty;
 import com.example.gypsophila.viewflipper.ViewFlipperAty;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     private Button fragment,viewPager,btn_viewFlipper,btn_scrollView,btn_gallery;
-    private Button btn_seekBar,btn_toast,btn_dialog,btn_notification;
+    private Button btn_seekBar,btn_toast,btn_dialog,btn_notification,btn_contextMenu;
+    private Button btn_sharedPreferences;
     private int mark;
 
     @Override
@@ -35,6 +41,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         btn_toast = (Button) findViewById(R.id.btn_toast);
         btn_dialog = (Button) findViewById(R.id.btn_dialog);
         btn_notification = (Button) findViewById(R.id.btn_notification);
+        btn_contextMenu = (Button) findViewById(R.id.btn_contextMenu);
+        btn_sharedPreferences = (Button) findViewById(R.id.btn_sharedPreferences);
         viewPager.setOnClickListener(this);
         btn_viewFlipper.setOnClickListener(this);
         btn_scrollView.setOnClickListener(this);
@@ -43,12 +51,41 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         btn_toast.setOnClickListener(this);
         btn_dialog.setOnClickListener(this);
         btn_notification.setOnClickListener(this);
+        btn_contextMenu.setOnClickListener(this);
+        btn_sharedPreferences.setOnClickListener(this);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        /**
+         * 加载xml方式
+         */
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        /**
+         * 代码动态加载
+         */
+//        menu.add(1, 1, 1, "菜单一");
+//        menu.add(1, 2, 1, "菜单二");
+
+        /**
+         * 子菜单,代码动态加载
+         */
+//        SubMenu file = menu.addSubMenu("文件");
+//        SubMenu edit = menu.addSubMenu("编辑");
+//        file.setHeaderTitle("文件操作");
+//        file.setHeaderIcon(R.mipmap.item1);
+//        file.add(1, 1, 1, "新建");
+//        file.add(1, 2, 1, "保存");
+//        file.add(1, 3, 1, "打开");
+//        edit.setHeaderTitle("编辑操作");
+//        edit.setHeaderIcon(R.mipmap.item4);
+//        edit.add(2, 1, 1, "复制");
+//        edit.add(2, 2, 1, "粘贴");
+//        edit.add(2, 3, 1, "重命名");
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_submenu, menu);
         return true;
     }
 
@@ -57,12 +94,101 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /**
+         * 加载xml
+         */
+        switch (item.getItemId()) {
+            case R.id.action_menu_item1: {
+                Toast.makeText(this, "点击了菜单一", Toast.LENGTH_SHORT).show();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                break;
+            }
+            case R.id.action_menu_item2: {
+                Toast.makeText(this, "点击了菜单二", Toast.LENGTH_LONG).show();
+                break;
+            }
         }
+
+        /**
+         * 代码动态加载
+         */
+//        switch (item.getItemId()) {
+//            case 1: {
+//                Toast.makeText(this, "点击了菜单一", Toast.LENGTH_SHORT).show();
+//                break;
+//            }
+//            case 2: {
+//                Toast.makeText(this, "点击了菜单二", Toast.LENGTH_LONG).show();
+//                break;
+//            }
+//        }
+//        if (item.getGroupId()==1) {
+//            switch (item.getItemId()) {
+//                case 1: {
+//                    Toast.makeText(this, "点击了新建", Toast.LENGTH_SHORT).show();
+//                    break;
+//                }
+//                case 2: {
+//                    Toast.makeText(this, "点击了保存", Toast.LENGTH_LONG).show();
+//                    break;
+//                }
+//                case 3: {
+//                    Toast.makeText(this, "点击了打开", Toast.LENGTH_LONG).show();
+//                    break;
+//                }
+//            }
+//        }
+//        else if (item.getGroupId() == 2) {
+//            switch (item.getItemId()) {
+//                case 1: {
+//                    Toast.makeText(this, "点击了复制", Toast.LENGTH_LONG).show();
+//                    break;
+//                }
+//                case 2: {
+//                    Toast.makeText(this, "点击了粘贴", Toast.LENGTH_LONG).show();
+//                    break;
+//                }
+//                case 3: {
+//                    Toast.makeText(this, "点击了重命名", Toast.LENGTH_LONG).show();
+//                    break;
+//                }
+//            }
+//        }
+
+        /**
+         * xml加载
+         */
+
+        switch (item.getItemId()) {
+            case R.id.submenu_item1: {
+                Toast.makeText(this, "点击了新建", Toast.LENGTH_SHORT).show();
+                //跳转页面
+                Intent intent = new Intent(MainActivity.this, SharedPreferencesAty.class);
+                item.setIntent(intent);
+                break;
+            }
+            case R.id.submenu_item2: {
+                Toast.makeText(this, "点击了保存", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.submenu_item3: {
+                Toast.makeText(this, "点击了打开", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.submenu_item4: {
+                Toast.makeText(this, "点击了复制", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.submenu_item5: {
+                Toast.makeText(this, "点击了粘贴", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.submenu_item6: {
+                Toast.makeText(this, "点击了重命名", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -94,23 +220,38 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             case R.id.btn_gallery: {
                 Intent intent = new Intent(this, GalleryAty.class);
                 startActivity(intent);
+                break;
             }
 
             case R.id.btn_seekBar: {
                 Intent intent = new Intent(this, SeekBarAty.class);
                 startActivity(intent);
+                break;
             }
             case R.id.btn_toast: {
                 Intent intent = new Intent(this, ToastAty.class);
                 startActivity(intent);
+                break;
             }
             case R.id.btn_dialog: {
                 Intent intent = new Intent(this, DialogAty.class);
                 startActivity(intent);
+                break;
             }
             case R.id.btn_notification: {
                 Intent intent = new Intent(this, NotificationAty.class);
                 startActivity(intent);
+                break;
+            }
+            case R.id.btn_contextMenu: {
+                Intent intent = new Intent(this, ContextMenuAty.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.btn_sharedPreferences: {
+                Intent intent = new Intent(this, SharedPreferencesAty.class);
+                startActivity(intent);
+                break;
             }
 
         }
