3ef8a55375f95143a437449a1c9a34c68b3d1d0c
Gyps0phila <chenjie.on1@qq.com>
2016-03-06 00:16:28 +0800

    activitycollector and baseaty

--- src/main/java/com/example/gypsophila/dailytest/BaseAty.java
+++ src/main/java/com/example/gypsophila/dailytest/BaseAty.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 /**
- * Created by Gypsophila on 2016/3/5.
+ * Created by Gypsophila on 2016/3/4.
  */
 public class BaseAty extends Activity {
 
@@ -13,5 +13,13 @@ public class BaseAty extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("BaseActivity", getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
     }
 }
